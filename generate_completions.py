ef8b815b1fcdbcf8f476f72d2aa4d76e36b27858
David Shapiro <daveshap37@gmail.com>
2022-06-01 07:02:01 -0400

    really in love with this prompt and results, running the full thing now

--- generate_completions.py
+++ generate_completions.py
@@ -16,7 +16,7 @@ def save(content, file):
 openai.api_key = open_file('openaiapikey.txt')
 
 
-def gpt3_completion(prompt, engine='text-curie-001', temp=0.7, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
     max_retry = 5
     retry = 0
     while True:
